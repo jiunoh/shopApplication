b0115c73865da26ff689b35bbc0e57635f32733e
jieunoh <juneoh227@gmail.com>
2018-09-15 15:30:00 +0900

    changed to jpa repository

--- src/main/java/com/shop/ShopDAO.java
+++ src/main/java/com/shop/ShopDAO.java
@@ -1,7 +1,9 @@
 package com.shop;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ShopDAO extends CrudRepository<ShopVO, Integer>  {
+public interface ShopDAO extends JpaRepository<ShopVO, Integer>  {
 	ShopVO findById(int id);
+	ShopVO findByName(String name);
+	ShopVO findByNameAndIsDeleted(String name, String isDeleted);
 }
