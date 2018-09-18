8e5cf58be14b1ba2dfd13b9ff0cd69988da228d4
jieunoh <juneoh227@gmail.com>
2018-09-18 16:14:06 +0900

    added shopService

--- src/main/java/com/shop/ShopController.java
+++ src/main/java/com/shop/ShopController.java
@@ -1,28 +1,22 @@
 package com.shop;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 
+import com.shop.ShopService;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ShopController {
+	
 	@Autowired
-	ShopDAO shopRepository;
+	ShopService shopService;
 	
 	@RequestMapping("/")
 	public String index() {
@@ -36,61 +30,40 @@ public class ShopController {
 	
 	@RequestMapping(value="/list")
 	public String getShopList(Model model) {
-		List<Shop> shopList = shopRepository.findByIsDeleted("n");
+		List<Shop> shopList = shopService.getShopList();
 		model.addAttribute("shopList", shopList);
 		return "list";
 	}
 	
 	@RequestMapping(value="/addShop")
 	public String addShop(@RequestBody Map<String, Object> shopInfo) {
-		Shop shop = new Shop();
-		shop.setName(shopInfo.get("shopName").toString());		
-		Calendar calendar = Calendar.getInstance();
-		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd-hh:mm");
-		String regDate = format.format(calendar.getTime());
-		shop.setRegDate(regDate);
-		shop.setModDate(regDate);
-		shop.setMenu(shopInfo.get("menu").toString());
-		shop.setIsDeleted("n");
-		shopRepository.save(shop);
+		shopService.addShop(shopInfo);
 		return "redirect:/list";
 	}
 	
 	@RequestMapping(value="deleteShop/{id}")
 	public String deleteShop(@PathVariable int id) {
-		Shop shop = shopRepository.findById(id);
-		shop.setIsDeleted("y");
-		shopRepository.save(shop);
+		shopService.deleteShop(id);
 		return "redirect:/list";
 	}
 	
 	@RequestMapping(value="/modification/{id}")
 	public String modification(Model model, @PathVariable int id) {
-		Shop shop = shopRepository.findById(id);
+		Shop shop = shopService.getShop(id);
 		model.addAttribute("shop", shop);
 		return "modification";
 	}
 	
 	@RequestMapping(value="/updateShop/{id}")
 	public String updateShop(@RequestBody Map<String, Object> shopInfo, @PathVariable int id) {
-		Shop shop = shopRepository.findById(id);
-		shop.setName(shopInfo.get("shopName").toString());		
-		Calendar calendar = Calendar.getInstance();
-		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd-hh:mm");
-		String regDate = format.format(calendar.getTime());
-		shop.setModDate(regDate);
-		shop.setMenu(shopInfo.get("menu").toString());
-		shopRepository.save(shop);
-		System.out.print(id+" "+shop.getName());
+		shopService.updateShop(shopInfo, id);
 		return "redirect:/list";
 	}
 	
 	@RequestMapping(value="/details/{id}")
 	public String getDetails(Model model, @PathVariable int id) {
-		Shop shop = shopRepository.findById(id);
-		Shop shop2 = shopRepository.findById(id);
+		Shop shop = shopService.getShop(id);
 		model.addAttribute("shop", shop);
-		model.addAttribute("shop2", shop2);
 		return "details";
 	}
 	
